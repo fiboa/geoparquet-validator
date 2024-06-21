e5c5ca1e4b6b59859e37e185e96e5b4a5a5fae65
Chris Holmes <cholmes@planet.com>
2024-06-20 17:21:59 -0700

    updated docs, added determination method

--- fiboa_cli/datasets/planet_afb.py
+++ fiboa_cli/datasets/planet_afb.py
@@ -7,13 +7,19 @@ import re
 SOURCES = None
 
 DATA_ACCESS = """
-Get the data from Planet through ...
+Data must be obtained from the Planet subscriptions API, see 
+https://developers.planet.com/docs/planetary-variables/field-boundaries/ for additional information.
+The output should look something like FIELD_BOUNDARIES_v1.0.0_S2_P1M-20230101T000000Z_fb.gpkg
 """
 
 ID = "planet_afb"
 SHORT_NAME = "Planet Field Boundaries"
 TITLE = "Field boundaries created by Planet's Automated Field Boundary detection algorithm"
-DESCRIPTION = "These field boundaries were originally created by Planet's automated field boundary detection algorithm, and converted to the fiboa format."
+DESCRIPTION = """
+These field boundaries are created by Planet Labs, using an automated process using satellite imagery. The algorithm
+works on a monthly basis and is available for the entire globe. The data is provided in GeoPackage format.
+For more information see the [field boundaries technical specification](https://planet.widen.net/s/5vq8w5wjvf/2403.08_mar-9444-field-boundaries-technical-specification-sheet-3)
+"""
 
 PROVIDER_NAME = "Planet Labs, PBC"
 PROVIDER_URL = "https://planet.com"
@@ -34,6 +40,10 @@ COLUMNS = {
     "ca_ratio": "ca_ratio", #custom field for Planet
     "micd": "micd", #custom field for Planet
     "qa": "qa", #custom field for Planet
+}
+
+ADD_COLUMNS = {
+  "determination_method": "auto-imagery"
 }
 
 EXTENSIONS = []
@@ -81,6 +91,7 @@ def convert(output_file, input_files = None, cache = None, source_coop_url = Non
         source_coop_url=source_coop_url,
         extensions=EXTENSIONS,
         missing_schemas=MISSING_SCHEMAS,
+        column_additions=ADD_COLUMNS,
         file_migration=FILE_MIGRATION,
         attribution=ATTRIBUTION,
         store_collection=collection,
